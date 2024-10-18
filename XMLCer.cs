d21745f8b4953f8af9af9571a6947f51ebc6b7e6
Gooten <Gootenmeister@users.noreply.github.com>
2024-10-18 10:46:02 +0200

    XMLClass

---
+++ XMLCer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PodApp // trelagersarkitektur - behövs annan namespace eller hur länkar man olika projs?
+{
+    [XmlSerializerAssembly] // kolla om rätt serializer
+    public class XMLCer
+    {
+
+        public void WriteXML()
+        {
+
+
+
+            ReadXML(); // laddar om efter den har skrivit någon ny data
+        }
+
+        public void ReadXML()
+        {
+
+        }
+
+
+    }
+}
